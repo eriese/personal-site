ca51c0b5fa2f9028b46e8aeb577f457aa9b2b8c3
Enoch Riese <enoch.riese@gmail.com>
2013-12-10 17:36:46 -0500

    made it look better

--- assets/js/alt.js
+++ assets/js/alt.js
@@ -1,23 +1,32 @@
 setSize = function() {
   var size;
-  if ($(".info")[0]) {
-    if($("body").width() < 750) {
-      $("table").css("display", "none");
-      $(".info").css("width", "70%");
-      $(".info").css("height", "");
-      $(".tint").css("padding", "15px 0px");
+
+  if($("body").width() < 750) {
+    $("table").css("display", "none");
+    $(".info").css("width", "70%");
+    $(".info").css("height", "");
+    $(".tint").css("padding", "15px 0px");
+  }
+  else {
+    if ($("body").width() < 1140) {
+      $(".img").css("width", "95%");
+      $(".description").css("width", "95%");
     }
     else {
-      size = $(window).width() * 0.15;
-      $("table").css("display", "");
-      $(".info").css("height", size);
-      $(".info").css("width", size);
-      $(".tint").css("padding-top", size / 2 - $(".text").height() / 2);
+      $(".img").css("width", "350px");
+      $(".description").css("width", "450px");
+    }
+    size = $(window).width() * 0.15;
+    $("table").css("display", "");
+    $(".info").css("height", size);
+    $(".info").css("width", size);
+    $(".tint").css("padding-top", size / 2 - $(".text").height() / 2);
+    if ($(".info")[0]) {
       var margin = $(".info").css("margin");
       var marginNum = margin.replace("px", "");
       $(".bottom").width(size + parseInt(marginNum) * 2);
-    };
-  }
+    }
+  };
   $("h1").parent().css("padding-top", $("h1").parent().height() / 2 - $("h1").height() / 2);
   // $("footer").css("margin-top", $(window).height() - $("footer").height());
 }
