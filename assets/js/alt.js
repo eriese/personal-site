c5b916eb594ad9a9ff38bec4fee684f71108577e
Enoch Riese <enoch.riese@gmail.com>
2013-12-11 15:48:13 -0500

    mobile responsive?

--- assets/js/alt.js
+++ assets/js/alt.js
@@ -1,33 +1,40 @@
 setSize = function() {
   var size;
-
-  if($("body").width() < 750) {
-    $("table").css("display", "none");
-    $(".info").css("width", "70%");
-    $(".info").css("height", "");
-    $(".tint").css("padding", "15px 0px");
+  if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+   $("table").css("display", "none");
+   $(".info").css("width", "70%");
+   $(".info").css("height", "");
+   $(".tint").css("padding", "15px 0px");
+   $(".img").css("width", "95%");
+   $(".description").css("width", "95%");
+ };
+ if($("body").width() < 750) {
+  $("table").css("display", "none");
+  $(".info").css("width", "70%");
+  $(".info").css("height", "");
+  $(".tint").css("padding", "15px 0px");
+}
+else {
+  if ($("body").width() < 1140) {
+    $(".img").css("width", "95%");
+    $(".description").css("width", "95%");
   }
   else {
-    if ($("body").width() < 1140) {
-      $(".img").css("width", "95%");
-      $(".description").css("width", "95%");
-    }
-    else {
-      $(".img").css("width", "350px");
-      $(".description").css("width", "450px");
-    }
-    size = $(window).width() * 0.15;
-    $("table").css("display", "");
-    $(".info").css("height", size);
-    $(".info").css("width", size);
-    $(".tint").css("padding-top", size / 2 - $(".text").height() / 2);
-    if ($(".info")[0]) {
-      var margin = $(".info").css("margin");
-      var marginNum = margin.replace("px", "");
-      $(".bottom").width(size + parseInt(marginNum) * 2);
-    }
-  };
-  $("h1").parent().css("padding-top", $("h1").parent().height() / 2 - $("h1").height() / 2);
+    $(".img").css("width", "350px");
+    $(".description").css("width", "450px");
+  }
+  size = $(window).width() * 0.15;
+  $("table").css("display", "");
+  $(".info").css("height", size);
+  $(".info").css("width", size);
+  $(".tint").css("padding-top", size / 2 - $(".text").height() / 2);
+  if ($(".info")[0]) {
+    var margin = $(".info").css("margin");
+    var marginNum = margin.replace("px", "");
+    $(".bottom").width(size + parseInt(marginNum) * 2);
+  }
+};
+$("h1").parent().css("padding-top", $("h1").parent().height() / 2 - $("h1").height() / 2);
   // $("footer").css("margin-top", $(window).height() - $("footer").height());
 }
 
@@ -53,6 +60,13 @@ $("#mouffe").on("click", function(e){
     $("#mouffe").css({"width": "100%"});
   }
 } );
+
+$(".info").on("mouseover", function(e){
+  $(this).addClass("hover")
+});
+$(".info").on("mouseout", function(e){
+  $(this).removeClass("hover")
+});
 // switchout = function(e) {
 //   e.preventDefault();
 //   var url = alert($(this).find("a").attr("href"));
