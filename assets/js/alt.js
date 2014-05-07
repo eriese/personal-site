ddff7074e00d0d06f2596d48c5fb0d639e69b6b6
Enoch Riese <enoch.riese@gmail.com>
2014-05-07 13:41:05 -0400

    animation works down, history works to one level

--- assets/js/alt.js
+++ assets/js/alt.js
@@ -1,4 +1,5 @@
 ARROWDIV = $("<div id='up-arrow'><a href='index.html'><img src='assets/images/arrow-up.png' alt='previous level'></a></div>")
+
 setSize = function() {
   var size;
   if($(window).width() > 750) {
@@ -35,11 +36,37 @@ var changeoutListener = function() {
   };
   return listenerCheck;
 }
+var addVideoListeners = function() {
+  $("video").on("click", function(e){
+    var video = $("video")[0];
+    if (video.paused) {
+      $(".img").css("width", "95%");
+      $("video").css({"width": "90%"});
+      video.play();
+    }
+    else {
+      video.pause();
+      $(".img").css("width", "");
+      $("video").css({"width": "100%"});
+    }
+  });
+}
+var addInfoListeners = function() {
+  $(".info").on({
+    mouseover: function(e){
+      $(this).addClass("hover");
+    },
+    mouseout: function(e){
+      $(this).removeClass("hover");
+    }
+  });
+}
 listenerCheck = false;
 var coverBases = function() {
   setSize();
   changeoutListener();
   addInfoListeners();
+  addVideoListeners();
 }
 
 $(window).on({ready: coverBases,
@@ -51,53 +78,21 @@ $(".footer").on("mouseout", function() {
   $(".footer").removeClass("hover");
 });
 
-$("#mouffe").on("click", function(e){
-  var video = $("#mouffe")[0];
-  if (video.paused) {
-    $(".img").css("width", "95%");
-    $("#mouffe").css({"width": "90%"});
-    video.play();
-  }
-  else {
-    video.pause();
-    $(".img").css("width", "");
-    $("#mouffe").css({"width": "100%"});
-  }
-} );
-var addInfoListeners = function() {
-  $(".info").on({
-    mouseover: function(e){
-      $(this).addClass("hover");
-    },
-    mouseout: function(e){
-      $(this).removeClass("hover");
-    }
-  });
-}
-
-// (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-//   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-//   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-// })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-// ga('create', 'UA-46992121-1', 'enochriese.com');
-// ga('send', 'pageview');
-getPage = function($div) {
-  var url = $thisDiv.find("a").attr("href");
-  $.ajax({
-    url: url,
-    type: "GET",
-    dataType : "html",
-
-  }).done(function(data) {
-    changeOut($div, data);
-  });
-}
-
 var infoClick = function(e) {
-  e.preventDefault();
+
   $thisDiv = $(this);
-  getPage($thisDiv);
+  if($thisDiv.find("a").attr("href").indexOf("http://") < 0) {
+    e.preventDefault();
+    var currHash = window.location.hash;
+    var url = $thisDiv.find("a").attr("href").replace(".html", "");
+    // debugger
+    if (currHash == "") {
+      window.location.hash = url;
+    }
+    else{
+      window.location.hash = currHash + "/" + url;
+    }
+  }
 }
 var fixInfoDivs = function() {
   posArray = []
@@ -109,27 +104,73 @@ var fixInfoDivs = function() {
     $(div).css(posArray[index]);
   });
 }
-var changeOut = function($thisDiv, data) {
+// (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+//   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+//   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+// })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+// ga('create', 'UA-46992121-1', 'enochriese.com');
+// ga('send', 'pageview');
+var cache = {
+  // If url is '' (no fragment), display this div's content.
+  '': $('.default')
+};
+getPage = function(url) {
+  var $thisDiv = $("a[href='" + url + ".html']").parent();
+  $.ajax({
+    url: url + ".html",
+    type: "GET",
+    dataType : "html",
+
+  }).done(function(data) {
+    var storageDiv = $("<div>");
+    storageDiv.append(data);
+    cache[url] = storageDiv;
+    changeOut($thisDiv, storageDiv);
+  });
+}
+$(window).on("hashchange", function(e) {
+  var uncutUrl = window.location.hash.replace("#", "");
+  urlArray = uncutUrl.split("/");
+  url = urlArray.slice(-1)[0];
+  if (url != "" ) {
+    $.each(urlArray, function(index, url){
+      if (cache[url]) {
+        var clickDiv = $("a[href='" + url + ".html']").parent();
+        changeOut(clickDiv, cache[url]);
+      }
+      else {
+        getPage(url);
+      }
+    });
+  }
+});
+
+$(window).trigger("hashchange");
+
+
+var changeOut = function($thisDiv, $newPage) {
   listenerCheck = false;
-  var blankDiv = $("<div>");
-  blankDiv.append(data);
+  var blankDiv = $newPage;
+  var storageDiv = $("<div>");
+  // blankDiv.append(data);
   var pos = $thisDiv.position();
   fixInfoDivs();
   var textContent = $thisDiv.find(".text").text();
-  $thisDiv.addClass("coder name")
+  var backgroundClass = blankDiv.find(".name").attr("class");
+  $thisDiv.addClass(backgroundClass)
   .removeClass("hover info")
   .css({"margin-left": 15})
   .find(".tint").html("<h1>" + textContent + "</h1>")
   .css({"padding-top": $thisDiv.height() / 2 - $thisDiv.find("h1").height() / 2});
-  $thisDiv.animate({height: 200, width: 200, "margin-left": 11, "margin-top": 19}, 300, function() {
+  $thisDiv.animate({height: 200, width: 200, "margin-left": 11, "margin-top": 19}, 500, function() {
     $thisDiv.off()
     .find("a").attr("href", "index.html")
-
     $thisDiv.find(".tint").css({"padding-top": $thisDiv.height() / 2 - $thisDiv.find("h1").height() / 2});
     var everythingElse = $("body").children().not($thisDiv).not(".footer")
     $.when(everythingElse.fadeOut(300)).done(function() {
-      $thisDiv.animate({top: 0, left: "50%", "margin-left": -100}, 500, function() {
-        everythingElse.remove();
+      $thisDiv.animate({top: 0, left: "50%", "margin-left": -100}, 700, function() {
+        storageDiv.append(everythingElse.remove());
         $("body").prepend(blankDiv.find("#up-arrow").hide());
         $("#up-arrow").fadeIn();
         var table = blankDiv.find("table");
@@ -143,10 +184,9 @@ var changeOut = function($thisDiv, data) {
             wrapper = label.find(".wrapper");
             wrapper.hide();
             $("body").append(label.hide());
-            label.fadeIn(500, function() {
-              wrapper.fadeIn(500);
-              coverBases();
-            });
+            label.show();
+            wrapper.slideDown(2000);
+            coverBases();
           });
         }
         else {
@@ -157,7 +197,6 @@ var changeOut = function($thisDiv, data) {
               $("#top").removeClass("to-scroll");
               coverBases();
               $("#bottom").animate({height: 80}, 500, function() {
-
                 info.fadeIn(500);
                 setSize();
                 addInfoListeners();
@@ -168,6 +207,7 @@ var changeOut = function($thisDiv, data) {
       });
 });
 });
+return storageDiv;
 }
 
 
