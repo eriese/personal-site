d69769b0de6f037412ccee14699f13ab3cef0c6b
Enoch Riese <enoch.riese@gmail.com>
2018-02-12 17:30:13 -0600

    setting up for es6 experiment with node

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+	entry: ['babel-polyfill', './assets/js/main.js'],
+	output: {
+		path: path.resolve(__dirname, './build'),
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			{
+				loader: 'babel-loader',
+				test: /\.js$/,
+				exclude: /node-modules/,
+				query: {
+					presets: ['env']
+				}
+			}
+		]
+	},
+	devServer: {
+		port: 3000,
+		contentBase: "./build",
+		inline: true
+	}
+}
