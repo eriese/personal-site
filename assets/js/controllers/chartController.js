e624f21dbcb43373338fc93b811fafc48810b24c
Enoch Riese <enoch.riese@gmail.com>
2018-02-15 01:44:00 -0600

    continuous drawing with timeline markers

--- assets/js/controllers/chartController.js
+++ assets/js/controllers/chartController.js
@@ -35,36 +35,41 @@ export default class ChartController {
     		let nextCols = $element.find(".next-col");
     		tl.set(nextCols.find(".grid-container>div"), {'border-width': "0px 0px 0px 0px"})
 
+			let prevMarker;
 			if (!isEven) {
 				let firstCol = angular.element(nextCols.get(middleInd));
-				tl.set(firstCol.find(".grid-container .right-half"), {clearProps: "border-width"});
-				tl.from(firstCol.find(".grid-container"), animLength, {height: 2});
-				tl.from(firstCol.find(".info"), animLength, {opacity: 0});
-				tl.set(firstCol.find(".grid-container .left-half"), {clearProps: "border-width"});
-				tl.from(firstCol.find(".grid-container"), animLength, {width: "3%", ease: Linear.easeIn});
+				prevMarker = "first";
+				tl.set(firstCol.find(".grid-container>div"), {clearProps: "border-width"}, prevMarker);
+				tl.from(firstCol.find(".grid-container"), animLength, {height: 2, width: "3%", ease: Linear.easeIn}, prevMarker);
+
+				prevMarker = "firstOver";
+				tl.from(firstCol.find(".info"), animLength, {opacity: 0}, prevMarker);
 			}
 
 			let lastInd = numNexts - 1;
+
 			for (let i = middleInd + 1; i < numNexts; i++) {
 				let right = angular.element(nextCols.get(i));
 				let left = angular.element(nextCols.get(lastInd - i));
 				let curCols = right.add(left);
 				let isLast = i == lastInd;
 
-				// let startPoint = "-=" + (isEven || i != middleInd + 1 ? animLength : 0);
-
 				if (!isLast) {
-					tl.set(curCols.find(".left-half"), {width: "0%"});
+					tl.set(curCols.find(".left-half"), {width: "0%"}, prevMarker);
 				}
 
-				tl.set(curCols.find(".grid-container>div"), {clearProps: "border-width"});
-				tl.from(curCols.find(".grid-container"), animLength, {width: "0%", ease: Linear.easeIn});
-				tl.from(curCols.find(".grid-container"), animLength, {height: 2});
-				tl.from(curCols.find(".info"), animLength, {opacity: 0});
+				tl.set(curCols.find(".grid-container>div"), {clearProps: "border-width"}, prevMarker);
+				tl.from(curCols.find(".grid-container"), animLength, {width: "0%", ease: Linear.easeIn}, prevMarker);
+
+				prevMarker = `pos${i}`;
+				tl.from(curCols.find(".grid-container"), animLength, {height: 2}, prevMarker);
 
 				if (!isLast) {
-					tl.to(curCols.find(".left-half"), animLength, {width: "50%", ease: Linear.easeIn});
+					tl.to(curCols.find(".left-half"), animLength, {width: "50%", ease: Linear.easeIn}, prevMarker);
 				}
+
+				prevMarker = `pos${i}b`
+				tl.from(curCols.find(".info"), animLength, {opacity: 0}, prevMarker);
 			}
     	}
 
